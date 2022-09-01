70a09cad231fb6b8177041bb5bc8668c9dc93c8f
Roxana Saucedo <kara.el2012@gmail.com>
2022-08-31 23:04:53 -0300

    Promesas V5

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -1,23 +1,59 @@
 import React from 'react'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
 
 const Item = ({Productos}) => {
   return (
-    
-    <div
-    style={{
+    <Card className='card' sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt=""
+        height="180"
+        width= "100"
+        image={Productos.img}
+        src={Productos.img}
+        
+
+      />
+      
+      <div
+      style={{
+        padding: '1rem',
+        display: 'inline',
+        justifyContent: 'center',
+        alignItems:'center',
+        fontSize: '12px'
+      }}>
+      
+      
+      </div>
+      <div
+      style={{
         marginBlockEnd:'1rem',
         marginInlineEnd:'0rem',
         padding: '1rem',
-        display: 'flex',
+        display: 'inline',
         justifyContent: 'center',
         alignItems:'center',
       }}>
-    {Productos.nombre}
-    {Productos.precio}
-    {Productos.img}
-    </div>
-    
-  )
+        <div size="small">{Productos.id}.-</div>
+        <div size="medium">{Productos.nombre}</div>
+        <div size="small">{Productos.describe}</div>
+        <div size="medium" > $ {Productos.precio}</div>
+        
+      
+        
+      </div>
+    </Card>
+  );
 }
-
+ 
+    
+  
 export default Item
